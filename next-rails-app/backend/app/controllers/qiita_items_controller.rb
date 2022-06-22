75b33ed1ddbc84018ee4cb5528c65dadd21f3e42
Ochi-Shoichi5539 <dekamintv@gmail.com>
2022-06-23 01:28:59 +0900

    WIP

---
+++ next-rails-app/backend/app/controllers/qiita_items_controller.rb
@@ -0,0 +1,8 @@
+class QiitaItemsController < ApplicationController
+  def index
+    response_json = QiitaApiClient.get_items
+
+    # レスポンスを簡略化するため、titleプロパティのみ返すようにしている
+    render json: response_json.map {|item| item.slice('title') }
+  end
+end
